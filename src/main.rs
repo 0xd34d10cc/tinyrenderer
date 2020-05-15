01ae761681414e7c78752c17353386379b2e9a4d
0xd34d10cc <0xd34d10cc@gmail.com>
2020-05-15 14:31:56 +0000

    Render texture

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,103 @@
-use glam::{vec3, Vec3};
-use wavefront_obj::obj::{self, ObjSet, Primitive, Vertex};
+use std::error::Error;
+
+use glam::{vec2, vec3, Vec2, Vec3};
+use wavefront_obj::obj::{self, ObjSet, Primitive, TVertex, Vertex};
 
 type Image = image::RgbImage;
 type Color = image::Rgb<u8>;
+type Texture = Image;
+type ZBuffer = image::GrayImage;
+type ZDepth = image::Luma<u8>;
 
+fn min(a: f32, b: f32) -> f32 {
+    if a > b {
+        b
+    } else {
+        a
+    }
+}
 
-// returns a minumal rectangle which contains triangle abc
+fn max(a: f32, b: f32) -> f32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
+
+fn clamp(x: f32, minval: f32, maxval: f32) -> f32 {
+    if x < minval {
+        return minval;
+    }
+
+    if x > maxval {
+        return maxval;
+    }
+
+    x
+}
+
+fn barycentric(a: Vec2, b: Vec2, c: Vec2, p: Vec2) -> Vec3 {
+    let xs = vec3(c.x() - a.x(), b.x() - a.x(), a.x() - p.x());
+    let ys = vec3(c.y() - a.y(), b.y() - a.y(), a.y() - p.y());
+    let u = xs.cross(ys);
+    // (u.y/u.z, u.x/u.z) are coordinates in (a, ab, ac) basis
+    // TODO: why are we dividing by z here?
+    vec3(1.0 - (u.x() + u.y()) / u.z(), u.y() / u.z(), u.x() / u.z())
+}
+
+// returns a minumal rectangle around triangle abc
 #[inline(always)]
 fn find_box(a: Vec3, b: Vec3, c: Vec3) -> ((usize, usize), (usize, usize)) {
-    let max = |a, b| if a > b { a } else { b };
-    let min = |a, b| if a < b { a } else { b };
-
     let min_x = min(a.x(), min(b.x(), c.x()));
     let min_y = min(a.y(), min(b.y(), c.y()));
 
     let max_x = max(a.x(), max(b.x(), c.x()));
     let max_y = max(a.y(), max(b.y(), c.y()));
 
-    ((min_x as usize, min_y as usize), (max_x as usize, max_y as usize))
+    let min_box = (min_x as usize, min_y as usize);
+    let max_box = (max_x as usize, max_y as usize);
+
+    (min_box, max_box)
 }
+
+fn in_triangle<F>(a: Vec3, b: Vec3, c: Vec3, mut f: F) where F: FnMut(usize, usize, Vec3) {
+    let (min, max) = find_box(a, b, c);
+
+    for y in min.1..(max.1 + 1) {
+        for x in min.0..(max.0 + 1) {
+            let bc = barycentric(a.truncate(), b.truncate(), c.truncate(), vec2(x as f32, y as f32));
+            if bc.x() < 0.0 || bc.y() < 0.0 || bc.z() < 0.0 {
+                continue;
+            }
+
+            f(x, y, bc);
+        }
+    }
+}
+
 
 struct Renderer {
     target: Image,
-    zbuffer: Vec<f32>,
+    zbuffer: ZBuffer,
 }
 
 impl Renderer {
     fn new(width: usize, height: usize) -> Self {
         Renderer {
             target: Image::new(width as u32, height as u32),
-            zbuffer: vec![std::f32::NEG_INFINITY; width * height],
+            zbuffer: ZBuffer::new(width as u32, height as u32),
         }
     }
 
     fn flipv(&mut self) {
         image::imageops::flip_vertical_in_place(&mut self.target);
+        image::imageops::flip_vertical_in_place(&mut self.zbuffer)
     }
 
-    fn save(&self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    fn save(&self, path: &str) -> Result<(), Box<dyn Error>> {
         self.target.save(path)?;
+        self.zbuffer.save("zbuffer.png")?;
         Ok(())
     }
 
@@ -47,118 +106,166 @@ impl Renderer {
         self.target.put_pixel(x as u32, y as u32, color);
     }
 
-    fn triangle(&mut self, a: Vec3, b: Vec3, c: Vec3, color: Color) {
-        let (min, max) = find_box(a, b, c);
+    fn triangle_texture(
+        &mut self,
+        a: Vec3,
+        b: Vec3,
+        c: Vec3,
+        uv0: Vec2,
+        uv1: Vec2,
+        uv2: Vec2,
+        texture: &Texture,
+        intensity: f32,
+    ) {
+        in_triangle(a, b, c, |x, y, bc| {
+            // TODO: WTF?
+            let z = a.z() * bc.x() + b.z() * bc.y() + c.z() * bc.z() + 0.5;
+            let z = clamp(z, 0.0, 255.0) as u8;
 
-        let barycentric = |p: Vec3| {
-            let xs = vec3(
-                c.x() - a.x(),
-                b.x() - a.x(),
-                a.x() - p.x(), // projection(pa, x)
-            );
-            let ys = vec3(
-                c.y() - a.y(),
-                b.y() - a.y(),
-                a.y() - p.y(),
-            );
-
-            let u = xs.cross(ys);
-            // (u.y/u.z, u.x/u.z) are coordinates in (a, ab, ac) basis
-            // TODO: why are we dividing by z here?
-
-            vec3(1.0 - (u.x() + u.y()) / u.z(), u.y() / u.z(), u.x() / u.z())
-        };
-
-        for y in min.1..(max.1 + 1) {
-            for x in min.0..(max.0 + 1) {
-                let p = vec3(x as f32, y as f32, 0.0);
-                let bc = barycentric(p);
-                if bc.x() < 0.0 || bc.y() < 0.0 || bc.z() < 0.0
-                {
-                    // (x, y) is out of triangle
-                    continue;
-                }
+            // if previous pixel put at |x, y| as further away from camera, replace it
+            let prev_z = self.zbuffer.get_pixel_mut(x as u32, y as u32);
+            if prev_z[0] <= z {
+                *prev_z = ZDepth::from([z]);
 
                 // TODO: WTF?
-                let z = bc.x() * a.z() + bc.y() * b.z() + bc.z() * c.z();
+                let uv = uv0 * bc.x() + uv1 * bc.y() + uv2 * bc.z();
+                let color = *texture.get_pixel(uv.x() as u32, uv.y() as u32);
+                let color = Color::from([
+                    (color[0] as f32 * intensity) as u8,
+                    (color[1] as f32 * intensity) as u8,
+                    (color[2] as f32 * intensity) as u8,
+                ]);
+                self.set(x, y, color);
+            }
+        });
+    }
 
-                // if previous pixel put at |x, y| as further away from camera, replace it
-                let prev_z = &mut self.zbuffer[x + y * self.target.width() as usize];
-                if *prev_z < z {
-                    *prev_z =  z;
-                    self.set(x, y, color);
+    fn triangle(&mut self, a: Vec3, b: Vec3, c: Vec3, color: Color) {
+        in_triangle(a, b, c, |x, y, bc| {
+            let z = a.z() * bc.x() + b.z() * bc.y() + c.z() * bc.z() + 0.5;
+            let z = clamp(z, 0.0, 255.0) as u8;
+            let prev_z = self.zbuffer.get_pixel_mut(x as u32, y as u32);
+            if prev_z[0] <= z {
+                *prev_z = ZDepth::from([z]);
+                self.set(x, y, color);
+            }
+        });
+    }
+
+    fn primitive(
+        &mut self,
+        primitive: &Primitive,
+        vertices: &[Vertex],
+        texture_vertices: &[TVertex],
+        texture: &Texture,
+    ) {
+        let width = self.target.width();
+        let height = self.target.height();
+
+        let translate3 = |vertex: Vec3| -> Vec3 {
+            // coordinates in obj file are in [-1.0; 1.0] range
+            // NOTE: not really, but it's true for african_head.obj
+            vec3(
+                (vertex.x() + 1.0) / 2.0 * (width - 1) as f32,
+                (vertex.y() + 1.0) / 2.0 * (height - 1) as f32,
+                vertex.z(),
+            )
+        };
+
+        let translate2 = |vertex: Vec2| -> Vec2 {
+            vec2(
+                vertex.x() * (texture.width() - 1) as f32,
+                vertex.y() * (texture.height() - 1) as f32,
+            )
+        };
+
+        let to_vec3 = |p: Vertex| -> Vec3 { vec3(p.x as f32, p.y as f32, p.z as f32) };
+        let to_vec2 = |p: TVertex| -> Vec2 { vec2(p.u as f32, p.v as f32) };
+
+        let light_direction = vec3(0.0, 0.0, -1.0);
+
+        match primitive {
+            Primitive::Triangle((x, Some(tx), _), (y, Some(ty), _), (z, Some(tz), _)) => {
+                let p1 = to_vec3(vertices[*x]);
+                let p2 = to_vec3(vertices[*y]);
+                let p3 = to_vec3(vertices[*z]);
+
+                let normal = (p3 - p1).cross(p2 - p1).normalize();
+                let intensity = normal.dot(light_direction);
+                if intensity.is_sign_positive() {
+                    let uv0 = to_vec2(texture_vertices[*tx]);
+                    let uv1 = to_vec2(texture_vertices[*ty]);
+                    let uv2 = to_vec2(texture_vertices[*tz]);
+                    self.triangle_texture(
+                        translate3(p1),
+                        translate3(p2),
+                        translate3(p3),
+                        translate2(uv0),
+                        translate2(uv1),
+                        translate2(uv2),
+                        texture,
+                        intensity,
+                    );
                 }
             }
+            Primitive::Triangle((x, _, _), (y, _, _), (z, _, _)) => {
+                let p1 = to_vec3(vertices[*x]);
+                let p2 = to_vec3(vertices[*y]);
+                let p3 = to_vec3(vertices[*z]);
+
+                let normal = (p3 - p1).cross(p2 - p1).normalize();
+                let intensity = normal.dot(light_direction);
+
+                let shade = (0xff as f32 * intensity) as u8;
+                let color = [shade, shade, shade].into();
+
+                if intensity.is_sign_positive() {
+                    self.triangle(
+                        translate3(p1),
+                        translate3(p2),
+                        translate3(p3),
+                        color
+                    );
+                }
+            },
+            _ => todo!(),
         }
     }
 
-    fn obj(&mut self, model: &ObjSet) {
-        let width = self.target.width();
-        let height = self.target.height();
-        let translate = |vertex: Vertex| -> Vec3 {
-            // coordinates in obj file are in [-1.0; 1.0] range
-            let x = (vertex.x as f32 + 1.0) / 2.0 * (width - 1) as f32;
-            let y = (vertex.y as f32 + 1.0) / 2.0 * (height - 1) as f32;
-            let z = vertex.z as f32;
-            vec3(x, y, z)
-        };
-
+    fn obj(&mut self, model: &ObjSet, texture: &Texture) {
         for object in &model.objects {
             for geometry in &object.geometry {
                 for shape in &geometry.shapes {
-                    match shape.primitive {
-                        Primitive::Triangle(x, y, z) => {
-                            let (x, y, z) = (x.0, y.0, z.0);
-
-                            let p1 = object.vertices[x];
-                            let p2 = object.vertices[y];
-                            let p3 = object.vertices[z];
-
-                            let to_vec3 =
-                                |p: Vertex| -> Vec3 { vec3(p.x as f32, p.y as f32, p.z as f32) };
-
-                            let normal = (to_vec3(p3) - to_vec3(p1))
-                                .cross(to_vec3(p2) - to_vec3(p1))
-                                .normalize();
-                            let light_direction = vec3(0.0, 0.0, -1.0);
-                            let intensity = normal.dot(light_direction);
-
-                            let shade = (0xff as f32 * intensity) as u8;
-                            let color = [shade, shade, shade].into();
-
-                            if intensity.is_sign_positive() {
-                                self.triangle(
-                                    translate(p1),
-                                    translate(p2),
-                                    translate(p3),
-                                    color,
-                                );
-                            }
-                        }
-                        _ => todo!(),
-                    }
+                    self.primitive(
+                        &shape.primitive,
+                        &object.vertices,
+                        &object.tex_vertices,
+                        &texture,
+                    );
                 }
             }
         }
     }
 }
 
-fn read_model(path: &str) -> Result<ObjSet, Box<dyn std::error::Error>> {
+fn read_model(path: &str) -> Result<ObjSet, Box<dyn Error>> {
     let model = std::fs::read_to_string(path)?;
     let model = obj::parse(&model)
         .map_err(|e| format!("Failed to parse line #{}: {}", e.line_number, e.message))?;
     Ok(model)
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let (width, height) = (800, 800);
     let mut renderer = Renderer::new(width, height);
 
-    let texture = image::open("obj/african_head_diffuse.png")?;
+    let mut texture = image::open("obj/african_head_diffuse.png")?.to_rgb();
+    image::imageops::flip_vertical_in_place(&mut texture);
+
     let model = read_model("obj/african_head.obj")?;
-    renderer.obj(&model);
+    renderer.obj(&model, &texture);
 
     renderer.flipv();
-    renderer.save("out.png")?;
+    renderer.save("target.png")?;
     Ok(())
 }
